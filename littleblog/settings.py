138080aca47c5630959fe2bf1d4010f011918693
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-04-02 11:37:20 +0100

    added new setting to config

--- littleblog/settings.py
+++ littleblog/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'littleblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('NAME'),
         'USER': env('USER'),
         'PASSWORD': env('PASSWORD'),
