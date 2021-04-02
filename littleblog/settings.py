77f20a2a215bae8cdccea50edac8be679ff48e39
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-04-02 12:21:38 +0100

    changeddatabase

--- littleblog/settings.py
+++ littleblog/settings.py
@@ -73,16 +73,22 @@ WSGI_APPLICATION = 'littleblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('RDS_DB_NAME'),
+#         'USER': env('RDS_USERNAME'),
+#         'PASSWORD': env('RDS_PASSWORD'),
+#         'HOST' : env('RDS_HOSTNAME'),
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('RDS_DB_NAME'),
-        'USER': env('RDS_USERNAME'),
-        'PASSWORD': env('RDS_PASSWORD'),
-        'HOST' : env('RDS_HOSTNAME'),
-        'PORT': 5432,
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 
